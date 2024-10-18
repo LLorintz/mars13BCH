fa33d9d8112c1f8c19c49ea9bebc99adfb78b5b9
LLorintz <lorintz.levente@mechwart.com>
2024-10-18 13:45:13 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,67 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState, useEffect } from 'react'
 import './app.css'
 import PlayerName from './components/PlayerName/PlayerName'
 import Resource from './components/Resource/Resource'
 function App() {
-const [Megacredit, setMegacreidt] = useState(0)
-const [MegacreditProduction, setMegacreditProduction] = useState(0)
-const handleMegacreditChange=(increment:number)=>{
-    setMegacreidt(Megacredit+increment)
+  const loadResource=(resource:string)=>{
+    const savedMegacredit = localStorage.getItem(resource);
+    return savedMegacredit ? Number(savedMegacredit) : 0;
+  }
+
+const [Megacredit, setMegacredit] = useState(loadResource("megacredit"))
+const [MegacreditProduction, setMegacreditProduction] = useState(loadResource("mproduction"))
+
+const [Steel, setSteel] = useState(loadResource('steel'))
+const [SteelProduction, setSteelProduction] = useState(loadResource("sproduction"))
+//
+//Titan
+const [Titan, setTitan] = useState(loadResource('titan'))
+const [TitanProduction, setTitanProduction] = useState(loadResource("sTitan"))
+//Plants
+//Energy
+//Heat
+
+const handleIncrement=(increment:number, resource:number, setResource:(vlaue:number)=>void)=>{
+  setResource(resource+increment)
 }
-const handleMegacreditProduction=(increment:number)=>{
-    setMegacreditProduction(MegacreditProduction+increment)
-}
+
+useEffect(()=>{
+  localStorage.setItem("megacredit", Megacredit.toString());
+  localStorage.setItem("steel", Steel.toString());
+  localStorage.setItem("mproduction", MegacreditProduction.toString());
+  localStorage.setItem("sproduction", SteelProduction.toString());
+  localStorage.setItem("titan", Titan.toString());
+  localStorage.setItem("tproduction", TitanProduction.toString());
+},[Megacredit, Steel, SteelProduction, MegacreditProduction,Titan, TitanProduction])
 
 const handlSubmit2=(e:FormEvent)=>{
     e.preventDefault()
-    setMegacreidt(Megacredit+MegacreditProduction)
-}
-
+    setMegacredit(Megacredit+MegacreditProduction)
+    setSteel(Steel+SteelProduction)
+  }
   return (
     <form onSubmit={handlSubmit2} className="container">
         <PlayerName></PlayerName>
         <Resource 
         name='Megacredit' 
         amount={Megacredit} 
-        onchange={handleMegacreditChange}
+        onchange={(increment:number)=>handleIncrement(increment, Megacredit, setMegacredit)}
         production={MegacreditProduction}
-        onchangeProductivity={handleMegacreditProduction}
+        onchangeProductivity={(increment:number)=>handleIncrement(increment, MegacreditProduction, setMegacreditProduction)}
+        ></Resource>
+        <Resource 
+        name='Steel' 
+        amount={Steel} 
+        onchange={(increment:number)=>handleIncrement(increment, Steel, setSteel)}
+        production={SteelProduction}
+        onchangeProductivity={(increment:number)=>handleIncrement(increment, SteelProduction, setSteelProduction)}
+        ></Resource>
+         <Resource 
+        name='Titan' 
+        amount={Titan} 
+        onchange={(increment:number)=>handleIncrement(increment, Titan, setTitan)}
+        production={TitanProduction}
+        onchangeProductivity={(increment:number)=>handleIncrement(increment, TitanProduction, setTitanProduction)}
         ></Resource>
        <button type='submit' className='NextRound'>Next Round</button>
     </form>
